c8324a2dfd2bdf9d971623f514f538817655d521
Sushil Gupta <kalwarsushil2@gmail.com>
2024-01-03 15:18:44 +0530

    todo mvc initial commit

---
+++ backend/src/model/mod.rs
@@ -0,0 +1,24 @@
+use thiserror::Error as ThisError;
+
+mod db;
+mod todo;
+
+// re-export
+pub use db::Db;
+pub use db::init_db;
+pub use todo::TodoMac;
+pub use todo::TodoPatch;
+pub use todo::TodoStatus;
+pub use todo::Todo;
+
+#[derive(ThisError, Debug)]
+pub enum Error{
+    #[error("Entity Not Found - {0}[{1}] ")]
+    EntityNotFound(&'static str, String),
+
+    #[error(transparent)]
+    SqlxError(#[from] sqlx::Error), 
+
+    #[error(transparent)]
+	IOError(#[from] std::io::Error),
+}
